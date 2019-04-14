2c43c906748a9153b5131c2b0e60d1f263754402
PARK Yong Seo <j@saro.me>
2019-04-14 13:54:38 +0900

    just clean import

--- src/main/kotlin/me/saro/ldap/jum/config/DBConfig.kt
+++ src/main/kotlin/me/saro/ldap/jum/config/DBConfig.kt
@@ -1,9 +1,7 @@
 package me.saro.ldap.jum.config
 
 import com.zaxxer.hikari.HikariDataSource
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.sql.DataSource
