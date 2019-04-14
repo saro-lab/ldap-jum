35e28bcdb73a8c5fd6a117f7053c5e528e8aa780
PARK Yong Seo <j@saro.me>
2019-04-14 15:44:53 +0900

    database config include hikaricp

--- src/main/kotlin/me/saro/ldap/jum/config/DBConfig.kt
+++ src/main/kotlin/me/saro/ldap/jum/config/DBConfig.kt
@@ -4,10 +4,12 @@ import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
 
 @Configuration
+@EnableTransactionManagement
 class DBConfig {
 
     @Value("\${datadir}") lateinit var datadir: String
@@ -22,6 +24,4 @@ class DBConfig {
         return conf
     }
 }
-
-
 
