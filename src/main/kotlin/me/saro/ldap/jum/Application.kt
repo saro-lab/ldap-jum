ff40ca5bc493ea69f28098066e28a7b56b2c3e61
PARK Yong Seo <j@saro.me>
2019-04-15 01:50:08 +0900

    application

--- src/main/kotlin/me/saro/ldap/jum/Application.kt
+++ src/main/kotlin/me/saro/ldap/jum/Application.kt
@@ -1,13 +1,25 @@
 package me.saro.ldap.jum
 
+import lombok.extern.slf4j.Slf4j
+import me.saro.ldap.jum.props.PropsService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("me.saro.ldap.jum.*")
+@Slf4j
 class Application : CommandLineRunner {
+
+	val log = LoggerFactory.getLogger(this.javaClass)!!
+
+	@Autowired lateinit var propsService: PropsService
+
 	override fun run(vararg args: String) {
-		println("start!!")
+        log.info("start server")
 	}
 }
 
