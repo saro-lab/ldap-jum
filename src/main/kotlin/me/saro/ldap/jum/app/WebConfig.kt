bbbb2cd7f5b422b2d18b204ed4a667ffc628f895
PARK Yong Seo <j@saro.me>
2019-05-26 22:59:21 +0900

    append install interceptor

--- src/main/kotlin/me/saro/ldap/jum/app/WebConfig.kt
+++ src/main/kotlin/me/saro/ldap/jum/app/WebConfig.kt
@@ -1,5 +1,8 @@
 package me.saro.ldap.jum.app
 
+import me.saro.ldap.jum.ldap.LdapService
+import me.saro.ldap.jum.ldap.LdapStatus
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.ModelAndView
@@ -12,17 +15,36 @@ import javax.servlet.http.HttpServletResponse
 @Configuration
 class WebConfig: WebMvcConfigurer {
 
+    @Autowired lateinit var ldapService: LdapService
+
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(this.noCacheInterceptor())
+        registry.addInterceptor(this.installInterceptor())
+        registry.addInterceptor(this.loginInterceptor())
     }
 
-    fun noCacheInterceptor(): HandlerInterceptor {
+    fun installInterceptor(): HandlerInterceptor {
         return object : HandlerInterceptor {
             override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any, modelAndView: ModelAndView?) {
-                val path = request!!.servletPath
-                if (path.matches("\\/[a-zA-Z]*".toRegex()) || path.startsWith("/oauth")) {
-                    response!!.setHeader("Cache-Control", "no-cache")
+                if (modelAndView != null) {
+                    if (request.servletPath.startsWith("/install")) {
+                        if (ldapService.status.equals(LdapStatus.ACTIVE)) {
+                            response.sendRedirect("/")
+                        }
+                    } else {
+                        if (ldapService.status.equals(LdapStatus.NOT_INSTALLED) || ldapService.status.equals(LdapStatus.CONNECTION_FAILURE)) {
+                            response.sendRedirect("/install")
+                        }
+                    }
                 }
+            }
+        }
+    }
+
+
+    fun loginInterceptor(): HandlerInterceptor {
+        return object : HandlerInterceptor {
+            override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any, modelAndView: ModelAndView?) {
+
             }
         }
     }
